41049e506fab467f9dc0869bd79fa56388bd5dbd
Dave Nugent <drnugent@gmail.com>
2018-03-16 15:28:12 -0700

    Updating Readme and default config file to clarify what the Org URL is and where to find it (this tripped me up during app creation.

--- custom-login/util/default-config.js
+++ custom-login/util/default-config.js
@@ -1,7 +1,7 @@
 export default {
   oidc: {
     clientId: '{clientId}',
-    issuer: 'https://{yourOktaDomain}.com/oauth2/default',
+    issuer: '{yourOktaOrgURL}/oauth2/default',
     redirectUri: 'http://localhost:8080/implicit/callback',
     scope: 'openid profile email',
   },
