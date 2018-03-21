de0df8f29e7ab0953ae3a1a207d476f920b76994
Vijet Mahabaleshwar <vijet.mahabaleshwar@okta.com>
2018-03-21 10:47:24 -0700

    Adding --fix to eslint

--- scripts/setup-env.js
+++ scripts/setup-env.js
@@ -10,18 +10,13 @@
  * See the License for the specific language governing permissions and limitations under the License.
  */
 
-/* eslint-disable consistent-return, no-console */
+/* eslint-disable no-console */
 
 'use strict';
 
 const fs = require('fs');
 const { execSync } = require('child_process');
 const path = require('path');
-
-function executeCommand(command) {
-  console.log(`Executing command ${command}`);
-  execSync(command);
-}
 
 function updateConfig(file) {
   if (process.env.ISSUER === undefined || process.env.CLIENT_ID === undefined) {
@@ -45,12 +40,12 @@ function cloneRepository(repository, directory) {
 
   const command = `git clone ${repository}`;
   console.log(`Cloning repository ${directory}`);
-  executeCommand(command);
+  execSync(command);
 }
 
 updateConfig(path.join(__dirname, '..', 'okta-hosted-login', '/src/.samples.config.js'));
 updateConfig(path.join(__dirname, '..', 'custom-login', '/src/.samples.config.js'));
 cloneRepository('https://github.com/okta/samples-nodejs-express-4.git', 'samples-nodejs-express-4');
-executeCommand(`cd ${path.join(__dirname, '..', 'samples-nodejs-express-4')} && npm install`);
+execSync(`cd ${path.join(__dirname, '..', 'samples-nodejs-express-4')} && npm install`);
 updateConfig(path.join(__dirname, '..', 'samples-nodejs-express-4', '.samples.config.json'));
 cloneRepository('https://github.com/okta/okta-oidc-tck.git', 'okta-oidc-tck');
