81770b69d8be89e8664d3c3ced44ecb50e0e5ff0
Vijet Mahabaleshwar <vijet.mahabaleshwar@okta.com>
2018-03-22 10:12:43 -0700

    Updating setup script to use PHP resource server

--- scripts/setup-env.js
+++ scripts/setup-env.js
@@ -18,22 +18,34 @@ const fs = require('fs');
 const { execSync } = require('child_process');
 const path = require('path');
 
-function executeCommand(command) {
-  console.log(`Executing command ${command}`);
-  execSync(command);
-}
-
 function updateConfig(file) {
   if (process.env.ISSUER === undefined || process.env.CLIENT_ID === undefined) {
     console.error('[ERROR] Please set the ISSUER and CLIENT_ID Environment variables');
     process.exit(1);
-    return;
   }
 
   const data = fs.readFileSync(file, 'utf8');
   let result = data.replace(/{clientId}/g, process.env.CLIENT_ID);
   result = result.replace(/https:\/\/{yourOktaDomain}.com\/oauth2\/default/g, process.env.ISSUER);
   fs.writeFileSync(file, result, 'utf8');
+}
+
+function updatePHPConfig() {
+  const resourceServerDir = path.join(__dirname, '..', 'samples-php', 'resource-server');
+  let command;
+  if (process.platform === 'win32') {
+    command = `copy ${path.join(resourceServerDir, '.env.dist')} ${path.join(resourceServerDir, '.env')}`;
+  } else {
+    command = `cp ${path.join(resourceServerDir, '.env.dist')} ${path.join(resourceServerDir, '.env')}`;
+  }
+  execSync(command);
+
+  const org = process.env.ISSUER.split('/oauth2');
+  const data = fs.readFileSync(path.join(resourceServerDir, '.env'), 'utf8');
+  let result = data.replace(/CLIENT_ID=/g, `CLIENT_ID=${process.env.CLIENT_ID}`);
+  result = result.replace(/REDIRECT_URI=/g, 'REDIRECT_URI=http://localhost:8080/implicit/callback');
+  result = result.replace(/ORGANIZATION_URL=/g, `ORGANIZATION_URL=${org[0]}/`);
+  fs.writeFileSync(path.join(resourceServerDir, '.env'), result, 'utf8');
 }
 
 function cloneRepository(repository, directory) {
@@ -45,12 +57,12 @@ function cloneRepository(repository, directory) {
 
   const command = `git clone ${repository}`;
   console.log(`Cloning repository ${directory}`);
-  executeCommand(command);
+  execSync(command);
 }
 
 updateConfig(path.join(__dirname, '..', 'okta-hosted-login', '/src/.samples.config.js'));
 updateConfig(path.join(__dirname, '..', 'custom-login', '/src/.samples.config.js'));
-cloneRepository('https://github.com/okta/samples-nodejs-express-4.git', 'samples-nodejs-express-4');
-executeCommand(`cd ${path.join(__dirname, '..', 'samples-nodejs-express-4')} && npm install`);
-updateConfig(path.join(__dirname, '..', 'samples-nodejs-express-4', '.samples.config.json'));
+cloneRepository('https://github.com/okta/samples-php.git', 'samples-php');
+execSync(`cd ${path.join(__dirname, '..', 'samples-php', 'resource-server')} && composer install --ignore-platform-reqs`);
+updatePHPConfig();
 cloneRepository('https://github.com/okta/okta-oidc-tck.git', 'okta-oidc-tck');
