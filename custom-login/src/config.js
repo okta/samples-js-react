d99a827d5d7079c5684139107ea40378c357a732
Aaron Granick <aaron.granick@okta.com>
2019-11-15 10:41:46 -0800

    use `window.location.origin` as base for redirect uri

--- custom-login/src/config.js
+++ custom-login/src/config.js
@@ -1,12 +1,14 @@
+/* global window */
 const CLIENT_ID = process.env.CLIENT_ID || '{clientId}';
 const ISSUER = process.env.ISSUER || 'https://{yourOktaDomain}.com/oauth2/default';
 const OKTA_TESTING_DISABLEHTTPSCHECK = process.env.OKTA_TESTING_DISABLEHTTPSCHECK || false;
+const REDIRECT_URI = `${window.location.origin}/implicit/callback`;
 
 export default {
   oidc: {
     clientId: CLIENT_ID,
     issuer: ISSUER,
-    redirectUri: 'http://localhost:8080/implicit/callback',
+    redirectUri: REDIRECT_URI,
     scopes: ['openid', 'profile', 'email'],
     pkce: true,
     disableHttpsCheck: OKTA_TESTING_DISABLEHTTPSCHECK,
