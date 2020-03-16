0866826465d5f317bcd7088acb5585399271b5e2
Vijet Mahabaleshwar <vijet.mahabaleshwar@okta.com>
2020-03-16 11:13:42 -0700

    Test-2

--- okta-hosted-login/src/Home.jsx
+++ okta-hosted-login/src/Home.jsx
@@ -55,28 +55,42 @@ const Home = () => {
       <div>
         <Header as="h1">PKCE Flow w/ Okta Hosted Login Page</Header>
 
-        { authState.isAuthenticated && !userInfo &&
-        <div>Loading user information...</div>
-        }
+        { authState.isAuthenticated && !userInfo
+        && <div>Loading user information...</div>}
 
-        {authState.isAuthenticated && userInfo &&
+        {authState.isAuthenticated && userInfo
+        && (
         <div>
-          <p>Welcome back, {userInfo.name}!</p>
+          <p>
+            Welcome back,
+            {userInfo.name}
+            !
+          </p>
           <p>
             You have successfully authenticated against your Okta org, and have been redirected back to this application.  You now have an ID token and access token in local storage.
-            Visit the <a href="/profile">My Profile</a> page to take a look inside the ID token.
+            Visit the
+            {' '}
+            <a href="/profile">My Profile</a>
+            {' '}
+            page to take a look inside the ID token.
           </p>
           <h3>Next Steps</h3>
           <p>Currently this application is a stand-alone front end application.  At this point you can use the access token to authenticate yourself against resource servers that you control.</p>
           <p>This sample is designed to work with one of our resource server examples.  To see access token authentication in action, please download one of these resource server examples:</p>
           <ul>
-            {resourceServerExamples.map(example => <li key={example.url}><a href={example.url}>{example.label}</a></li>)}
+            {resourceServerExamples.map((example) => <li key={example.url}><a href={example.url}>{example.label}</a></li>)}
           </ul>
-          <p>Once you have downloaded and started the example resource server, you can visit the <a href="/messages">My Messages</a> page to see the authentication process in action.</p>
+          <p>
+            Once you have downloaded and started the example resource server, you can visit the
+            <a href="/messages">My Messages</a>
+            {' '}
+            page to see the authentication process in action.
+          </p>
         </div>
-        }
+        )}
 
-        {!authState.isAuthenticated &&
+        {!authState.isAuthenticated
+        && (
         <div>
           <p>If you&lsquo;re viewing this page then you have successfully started this React application.</p>
           <p>
@@ -92,7 +106,7 @@ const Home = () => {
           </p>
           <Button id="login-button" primary onClick={login}>Login</Button>
         </div>
-        }
+        )}
 
       </div>
     </div>
