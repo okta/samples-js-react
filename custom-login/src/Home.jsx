33bd9ad7e7915dad2db0f61898571d7835bee0fc
Aaron Granick <aaron.granick@okta.com>
2020-11-10 17:00:56 -0800

    Update for SIW 5.0

--- custom-login/src/Home.jsx
+++ custom-login/src/Home.jsx
@@ -64,7 +64,7 @@ const Home = () => {
         && (
         <div>
           <p>
-            Welcome back,
+            Welcome back,&nbsp;
             {userInfo.name}
             !
           </p>
