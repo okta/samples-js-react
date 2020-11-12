a255cd12249735fde8276a0e18d5e062835211c9
Vijet M <vijet.mahabaleshwar@okta.com>
2020-11-11 22:04:49 -0800

    Adds some additional env vars + java 1.8 upgrade

--- scripts/e2e.sh
+++ scripts/e2e.sh
@@ -2,7 +2,14 @@
 
 source ${OKTA_HOME}/${REPO}/scripts/setup.sh
 
-setup_service google-chrome-stable 83.0.4103.61-1
+setup_service xvfb start
+setup_service java 1.8.222
+setup_service google-chrome-stable 85.0.4183.102-1
+
+yum -y install lsof
+
+export TRAVIS=true
+export DBUS_SESSION_BUS_ADDRESS=/dev/null
 
 export TEST_SUITE_TYPE="junit"
 export TEST_RESULT_FILE_DIR="${REPO}/build2/e2e"
@@ -14,7 +21,14 @@ get_secret prod/okta-sdk-vars/password PASSWORD
 
 cd ${OKTA_HOME}/${REPO}
 
-if ! npm run test; then
+function run_tests() {
+    npm run pretest
+    # npm run test:okta-hosted-login
+    # kill -s TERM $(lsof -t -i:8080 -sTCP:LISTEN)
+    npm run test:custom-login
+}
+
+if ! run_tests; then
   echo "e2e tests failed! Exiting..."
   exit ${TEST_FAILURE}
 fi
