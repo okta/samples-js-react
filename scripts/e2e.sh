959bba1ce289f8974b7df176badb3f7b948e916a
Kostya <kostiantyn.drozdov@okta.com>
2021-02-09 16:57:00 +0200

    Change test result dir.

--- scripts/e2e.sh
+++ scripts/e2e.sh
@@ -12,7 +12,7 @@ export TRAVIS=true
 export DBUS_SESSION_BUS_ADDRESS=/dev/null
 
 export TEST_SUITE_TYPE="junit"
-export TEST_RESULT_FILE_DIR="${REPO}/build2/e2e"
+export TEST_RESULT_FILE_DIR="${REPO}/build2/reports"
 
 export ISSUER=https://samples-javascript.okta.com/oauth2/default
 export CLIENT_ID=0oapmwm72082GXal14x6
@@ -35,8 +35,7 @@ if ! run_tests; then
   echo "e2e tests failed! Exiting..."
   exit ${TEST_FAILURE}
 fi
-cd build2
-ls -a
+
 echo ${TEST_SUITE_TYPE} > ${TEST_SUITE_TYPE_FILE}
 echo ${TEST_RESULT_FILE_DIR} > ${TEST_RESULT_FILE_DIR_FILE}
 exit ${PUBLISH_TYPE_AND_RESULT_DIR}
