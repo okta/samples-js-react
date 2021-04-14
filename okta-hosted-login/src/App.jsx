42ea44b8bc68086d06182227c26c43fbb81e2e49
Denys Oblohin <denys.oblohin@okta.com>
2021-04-14 05:39:07 +0000

    Update to okta-react 5
    OKTA-377863
    <<<Jenkins Check-In of Tested SHA: c02373f316040b9b6f10c632a221fd18a7ea4329 for eng_productivity_ci_bot_okta@okta.com>>>
    Artifact: samples-js-react
    Files changed count: 7

--- okta-hosted-login/src/App.jsx
+++ okta-hosted-login/src/App.jsx
@@ -11,8 +11,8 @@
  */
 
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import { OktaAuth } from '@okta/okta-auth-js';
+import { Route, Switch, useHistory } from 'react-router-dom';
+import { OktaAuth, toRelativeUrl } from '@okta/okta-auth-js';
 import { Security, SecureRoute, LoginCallback } from '@okta/okta-react';
 import { Container } from 'semantic-ui-react';
 import config from './config';
@@ -26,22 +26,25 @@ const oktaAuth = new OktaAuth(config.oidc);
 
 const App = () => {
   const [corsErrorModalOpen, setCorsErrorModalOpen] = React.useState(false);
+  const history = useHistory();
+  const restoreOriginalUri = async (_oktaAuth, originalUri) => {
+    history.replace(toRelativeUrl(originalUri, window.location.origin));
+  };
 
   return (
-    <Router>
-      <Security oktaAuth={oktaAuth}>
-        <Navbar {...{ setCorsErrorModalOpen }} />
-        <CorsErrorModal {...{ corsErrorModalOpen, setCorsErrorModalOpen }} />
-        <Container text style={{ marginTop: '7em' }}>
-          <Switch>
-            <Route path="/" exact component={Home} />
-            <Route path="/login/callback" component={LoginCallback} />
-            <SecureRoute path="/messages" component={Messages} />
-            <SecureRoute path="/profile" component={Profile} />
-          </Switch>
-        </Container>
-      </Security>
-    </Router>
+    <Security oktaAuth={oktaAuth} restoreOriginalUri={restoreOriginalUri}>
+      <Navbar {...{ setCorsErrorModalOpen }} />
+      <CorsErrorModal {...{ corsErrorModalOpen, setCorsErrorModalOpen }} />
+      <Container text style={{ marginTop: '7em' }}>
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/login/callback" component={LoginCallback} />
+          <SecureRoute path="/messages" component={Messages} />
+          <SecureRoute path="/profile" component={Profile} />
+        </Switch>
+      </Container>
+    </Security>
   );
 };
+
 export default App;
