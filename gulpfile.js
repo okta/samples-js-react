35924f29e7564c33164f5fd52bb28ced541ffce0
Shuo Wu <wushuo2010@gmail.com>
2021-09-13 15:18:59 -0400

    add okta-react version to sample package.json

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,47 @@
 const { series, src, dest, parallel } = require('gulp');
 const clean = require('gulp-clean');
+const fs = require('fs');
 const shell = require('shelljs');
 
 const cleanDir = dir => () => {
   return src(dir, { read: false, allowEmpty: true })
     .pipe(clean({ force: true }));
-}
+};
 
 const cloneOktaReact = (done) => {
   shell.exec('git clone --single-branch --branch master https://github.com/okta/okta-react.git');
   done();
-}
+};
 
-const copySample = sample =>() => {
+const copySample = sample => () => {
   return src(`okta-react/generated/samples/${sample}/**/*`)
     .pipe(dest(`${sample}/`));
-}
+};
+
+const getPublishedModuleVersion = (module, cb) => {
+  const stdout = shell.exec(`yarn info ${module} dist-tags --json`, { silent: true });
+  const distTags = JSON.parse(stdout);
+  const version = distTags.data.latest;
+  console.log(`Last published ${module} version: `, version);
+  cb && cb();
+  return version;
+};
+
+
+const updateOktaReactVersion = sample => (done) => {
+  const oktaReactVersion = getPublishedModuleVersion('@okta/okta-react');
+  console.log(oktaReactVersion);
+  const packageJSON = JSON.parse(fs.readFileSync(`./${sample}/package.json`));
+  packageJSON.dependencies['@okta/okta-react'] = `^${oktaReactVersion}`;
+  fs.writeFileSync(`./${sample}/package.json`, JSON.stringify(packageJSON, null, 2));
+  console.log(packageJSON);
+  done();
+};
+
+const installTask = done => {
+  shell.exec('npm install');
+  done();
+};
 
 module.exports = {
   'pull-samples': series(
@@ -29,6 +55,11 @@ module.exports = {
       copySample('okta-hosted-login'),
       copySample('custom-login')
     ),
-    cleanDir('okta-react')
+    parallel(
+      updateOktaReactVersion('okta-hosted-login'),
+      updateOktaReactVersion('custom-login')
+    ),
+    cleanDir('okta-react'),
+    installTask
   )
 };
