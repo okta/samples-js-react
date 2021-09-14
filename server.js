9f56d2999e306472bb2d039b31f45ac30e90e543
Shuo Wu <wushuo2010@gmail.com>
2021-09-14 16:09:30 -0400

    load readme for root page

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const path = require('path');
+const fsPromises = require('fs').promises
+const marked = require('marked');
 
 const app = express();
 const port = process.env.PORT || 8080;
@@ -8,13 +10,14 @@ app.use('/okta-hosted-login', express.static(path.join(__dirname, 'okta-hosted-l
 
 app.use('/custom-login', express.static(path.join(__dirname, 'custom-login/build')));
 
-app.get('*', (req, res) => {
+app.use('*', (req, res) => {
   if (req.path.startsWith('/okta-hosted-login')) {
     res.sendFile(path.resolve(__dirname, 'okta-hosted-login', 'build', 'index.html'));
   } else if (req.path.startsWith('/custom-login')) {
     res.sendFile(path.resolve(__dirname, 'cusotm-login', 'build', 'index.html'));
   } else if (req.path === '/') {
-    res.send('Root page');
+    fsPromises.readFile(path.resolve(__dirname, 'README.md'), 'utf8')
+      .then(file => res.send(marked(file.toString(), { gfm: true })));
   } else {
     res.status(404).send('Not Found');
   }
