038905b828de629b0d2db2e778a60f623fb08b63
Jared Perreault <jared.perreault@okta.com>
2022-11-03 10:04:09 -0400

    fixes downstream script

--- scripts/downstream/create-downstream-for-authjs.sh
+++ scripts/downstream/create-downstream-for-authjs.sh
@@ -8,7 +8,7 @@ if [[ -z "${upstream_artifact_version}" ]]; then
   echo "okta-auth-js version that will be tested: ${upstream_artifact_version}"
 fi
 
-pushd ${OKTA_HOME}/okta-react/scripts > /dev/null
+pushd ${OKTA_HOME}/samples-js-react/scripts > /dev/null
 sdk_version_number="$(echo ${upstream_artifact_version} | cut -d'@' -f3)"
 echo "Update okta-auth-js version in scripts/setup.sh to ${sdk_version_number}"
 sed -i "s/\(AUTHJS_VERSION\=\).*/\1\"${sdk_version_number}\"/g" setup.sh
