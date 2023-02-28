581746df6107b07314a7432df44bb9d31220c53a
Jared Perreault <90656038+jaredperreault-okta@users.noreply.github.com>
2023-02-28 17:14:27 -0500

    dependency updates (#297)
    OKTA-565312 updates deps

--- scripts/setup-env.js
+++ scripts/setup-env.js
@@ -33,7 +33,13 @@ function cloneRepository(repository, directory, branch) {
   execSync(command);
 }
 
+function installDependencies(directory) {
+  const dir = path.resolve(__dirname, '..', directory);
+  execSync(`cd ${dir} && npm install`);
+}
+
 cloneRepository('https://github.com/okta/samples-nodejs-express-4.git', 'samples-nodejs-express-4');
 execSync(`cd ${path.join(__dirname, '..', 'samples-nodejs-express-4')} && npm install --unsafe-perm`);
 
 cloneRepository('https://github.com/okta/okta-oidc-tck.git', 'okta-oidc-tck', 'master');
+installDependencies('okta-oidc-tck/e2e-tests');
