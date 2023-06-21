2a68d44b7c77416660ef2f29aba5579a70f55791
denys.oblohin <denys.oblohin@okta.com>
2023-06-21 19:36:48 +0300

    Access secrets from Vault. Remove unused secrets

--- scripts/e2e/e2e-classic.sh
+++ scripts/e2e/e2e-classic.sh
@@ -6,6 +6,6 @@ export ISSUER=https://samples-javascript.okta.com/oauth2/default
 export CLIENT_ID=0oapmwm72082GXal14x6
 export SPA_CLIENT_ID=0oapmwm72082GXal14x6
 export USERNAME=george@acme.com
-get_secret prod/okta-sdk-vars/password PASSWORD
+get_vault_secret_key devex/samples-javascript password PASSWORD
 
 run_tests
--- scripts/e2e/e2e-oie.sh
+++ scripts/e2e/e2e-oie.sh
@@ -9,11 +9,6 @@ export CLIENT_ID=0oa8lrg7ojTsbJgRQ696
 export SPA_CLIENT_ID=0oa8lrg7ojTsbJgRQ696
 export USERNAME=george@acme.com
 export EMAIL_MFA_USERNAME=email-login@email.ghostinspector.com
-get_secret prod/okta-sdk-vars/password PASSWORD
-export SMS_MFA_USERNAME=sms-login@okta.com
-export FB_USERNAME=ycfjikukbl_1613767309@tfbnw.net
-get_secret prod/okta-sdk-vars/fb_password FB_PASSWORD
-get_secret prod/okta-sdk-vars/twilio_account TWILIO_ACCOUNT
-get_secret prod/okta-sdk-vars/twilio_api_token TWILIO_API_TOKEN
+get_vault_secret_key devex/samples-javascript password PASSWORD
 
 run_tests
