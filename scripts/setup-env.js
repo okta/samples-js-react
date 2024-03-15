a0c0cd42cdea0363a94b44cdf80e445664c6ef21
Jared Perreault <90656038+jaredperreault-okta@users.noreply.github.com>
2024-03-15 18:24:20 -0400

    dep update (#310)
    OKTA-681752 bumps axios

--- scripts/setup-env.js
+++ scripts/setup-env.js
@@ -42,4 +42,5 @@ cloneRepository('https://github.com/okta/samples-nodejs-express-4.git', 'samples
 execSync(`cd ${path.join(__dirname, '..', 'samples-nodejs-express-4')} && npm install --unsafe-perm`);
 
 cloneRepository('https://github.com/okta/okta-oidc-tck.git', 'okta-oidc-tck', 'master');
+execSync(`cd ${path.join(__dirname, '..', 'okta-oidc-tck')} && git checkout 25db149cac9f678a553d0975966eebc0724a4ad7`);   // workaround
 installDependencies('okta-oidc-tck/e2e-tests');
