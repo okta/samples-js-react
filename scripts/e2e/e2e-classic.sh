1487191611c90441f8f3b4594579bcc1d281abbc
Jared Perreault <jared.perreault@okta.com>
2024-10-16 11:35:26 -0400

    chore: ci update

--- scripts/e2e/e2e-classic.sh
+++ scripts/e2e/e2e-classic.sh
@@ -6,6 +6,6 @@ export ISSUER=https://samples-javascript.okta.com/oauth2/default
 export CLIENT_ID=0oapmwm72082GXal14x6
 export SPA_CLIENT_ID=0oapmwm72082GXal14x6
 export USERNAME=george@acme.com
-get_vault_secret_key devex/samples-javascript password PASSWORD
+get_terminus_secret "/" password PASSWORD
 
 run_tests
